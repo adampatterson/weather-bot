74b68ac36f428e1cafe59173589c98886110c985
Adam Patterson <hello@adampatterson.ca>
2018-10-26 14:02:07 -0600

    Adds Weather Model

---
+++ database/migrations/2018_10_26_194710_create_iata_weather_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIataWeatherTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('iata_weather', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string( 'key' )->index();
+            $table->string("name");
+            $table->string("city");
+            $table->string("country");
+            $table->string("iata");
+            $table->string("icao");
+            $table->string("latitude");
+            $table->string("longitude");
+            $table->string("altitude");
+            $table->string("timezone");
+            $table->string("dst");
+w
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('iata_weather');
+    }
+}
