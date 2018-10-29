4d47f5bbdd909ece05b62721325828280329ab75
Adam Patterson <hello@adampatterson.ca>
2018-10-29 16:42:36 -0600

    Cleanup.

--- routes/botman.php
+++ routes/botman.php
@@ -10,8 +10,14 @@ $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
 
-$botman->hears('Weather', function ($bot) {
-    $bot->reply("23c and ☀️");
+$botman->hears('Whats {var}', function ($bot, $var) {
+    $weatherService = new \App\WeatherBot\WeatherService;
+
+    $code = substr($var, -3);
+
+    $message = $weatherService->makeMessage($code);
+
+    $bot->reply($message);
 });
 
 $botman->hears('/weather {var}', function ($bot, $var) {
@@ -22,27 +28,4 @@ $botman->hears('/weather {var}', function ($bot, $var) {
     $message = $weatherService->makeMessage($code);
 
     $bot->reply($message);
-});
-
-$botman->hears('/lookup {var}', function ($bot, $var) {
-    $response = file_get_contents('https://www.goauto.ca/inventory/search/json?stock_number=' . $var);
-    $response = json_decode($response);
-
-    $vehicle = $response->results[0];
-    $price   = $vehicle->list_price;
-
-    $bodyStyle = $vehicle->body_type_name;
-
-    $bodyStyle = ($bodyStyle == 'Sedan') ? '🚘' : '🚙';
-
-    $bot->reply('This ' . $bodyStyle . ' is ' . Numeral::number($price)->format('$0,0.00'));
-});
-
-$botman->hears('Lets Chat', BotManController::class . '@startConversation');
-
-$botman->hears('Hello BotMan!', function ($bot) {
-    $bot->reply('Hello!');
-    $bot->ask('Whats your name?', function ($answer, $bot) {
-        $bot->say('Welcome ' . $answer->getText());
-    });
 });
